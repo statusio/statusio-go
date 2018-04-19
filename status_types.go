5c2ab8c2cc33ac1c8b4caf003cbc07f8a45a2fe7
Joe Nicosia <joe@t3ch.com>
2018-04-19 14:59:33 -0400

    Added missing structure elements for StatusSummary.

--- status_types.go
+++ status_types.go
@@ -11,6 +11,9 @@ type StatusSummaryResponse struct {
 		Status []struct {
 			ID         string `json:"id"`
 			Name       string `json:"name"`
+			Updated    time.Time `json:"updated"`
+			Status     string    `json:"status"`
+			StatusCode int       `json:"status_code"`
 			Containers []struct {
 				ID         string    `json:"id"`
 				Name       string    `json:"name"`
@@ -19,5 +22,75 @@ type StatusSummaryResponse struct {
 				StatusCode int       `json:"status_code"`
 			} `json:"containers"`
 		} `json:"status"`
+		StatusOverall struct {
+			Updated    time.Time `json:"updated"`
+			Status     string    `json:"status"`
+			StatusCode int       `json:"status_code"`
+		} `json:"status_overall"`
+		Incidents []struct {
+			ID         string `json:"_id"`
+			Name       string `json:"name"`
+			DateTimeOpen    time.Time `json:"datetime_open"`
+			Status     string    `json:"status"`
+			StatusCode int       `json:"status_code"`
+			Messages []struct {
+				Details       string    `json:"details"`
+				DateTime    time.Time `json:"datetime"`
+				State 		 int       `json:"state"`
+				Status     int    	 `json:"status"`
+			} `json:"messages"`
+			Components []struct {
+				ID         string    `json:"_id"`
+				Name       string    `json:"name"`
+			} `json:"components_affected"`
+			Containers []struct {
+				ID         string    `json:"_id"`
+				Name       string    `json:"name"`
+			} `json:"containers_affected"`
+		} `json:"incidents"`
+		Maintenance struct {
+			Active []struct {
+				ID         string `json:"_id"`
+				Name       string `json:"name"`
+				DateTimeOpen    time.Time `json:"datetime_open"`
+				DateTimeStart    time.Time `json:"datetime_planned_start"`
+				DateTimeEnd    time.Time `json:"datetime_planned_end"`
+				Messages []struct {
+					Details       string    `json:"details"`
+					DateTime    time.Time `json:"datetime"`
+					State 		 int       `json:"state"`
+					Status     int    	 `json:"status"`
+				} `json:"messages"`
+				Components []struct {
+					ID         string    `json:"_id"`
+					Name       string    `json:"name"`
+				} `json:"components_affected"`
+				Containers []struct {
+					ID         string    `json:"_id"`
+					Name       string    `json:"name"`
+				} `json:"containers_affected"`
+			} `json:"active"`
+			Upcoming []struct {
+				ID         string `json:"_id"`
+				Name       string `json:"name"`
+				DateTimeOpen    time.Time `json:"datetime_open"`
+				DateTimeStart    time.Time `json:"datetime_planned_start"`
+				DateTimeEnd    time.Time `json:"datetime_planned_end"`
+				Messages []struct {
+					Details       string    `json:"details"`
+					DateTime    time.Time `json:"datetime"`
+					State 		 int       `json:"state"`
+					Status     int    	 `json:"status"`
+				} `json:"messages"`
+				Components []struct {
+					ID         string    `json:"_id"`
+					Name       string    `json:"name"`
+				} `json:"components_affected"`
+				Containers []struct {
+					ID         string    `json:"_id"`
+					Name       string    `json:"name"`
+				} `json:"containers_affected"`
+			} `json:"upcoming"`
+		} `json:"maintenance"`
 	} `json:"result"`
 }
